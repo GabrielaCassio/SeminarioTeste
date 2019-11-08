2a339b9693e6eb46a6c5eae80e8a441d907d1caf
Luiz <luizfelippee98@gmail.com>
2019-11-07 21:38:00 -0300

    adição do projeto de teste unitario simples em c#

---
+++ TesteUnitarioSimples/SeminarioTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeminarioTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeminarioTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79ace042-ff59-41dc-8e6a-177e03ecc013")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TesteUnitarioSimples/SeminarioTeste/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeminarioTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.sistemas.pucminas.br/sgaaluno3/SilverStream/Pages/pgAln_LoginSSL.html");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(1000);
+            IWebElement matricula = driver.FindElement(By.Name("S48_"));
+            matricula.SendKeys("600049");
+            System.Threading.Thread.Sleep(1000);
+
+            IWebElement senha = driver.FindElement(By.Name("S62_"));
+            senha.SendKeys("calor");
+            System.Threading.Thread.Sleep(1000);
+
+            IWebElement tipo = driver.FindElement(By.Name("S76_"));
+            tipo.SendKeys("g");
+            System.Threading.Thread.Sleep(1000);
+
+            IWebElement clicar = driver.FindElement(By.Name("S122_"));
+            clicar.Click();
+
+
+            System.Threading.Thread.Sleep(5000);
+            driver.Quit();
+
+        }
+    }
+}
