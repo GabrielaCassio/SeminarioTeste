e79c479a98a0e1fd2a7bf9e84292358f9fc54b70
luizfelipe0 <33462370+luizfelipe0@users.noreply.github.com>
2019-11-08 21:50:36 -0300

    Correção de parâmetro
    Atual Linha 25 do código - Alterando para um parâmetro genérico e informativo

--- TesteUnitarioSimples/SeminarioTeste/UnitTest1.cs
+++ TesteUnitarioSimples/SeminarioTeste/UnitTest1.cs
@@ -13,23 +13,24 @@ namespace SeminarioTeste
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.sistemas.pucminas.br/sgaaluno3/SilverStream/Pages/pgAln_LoginSSL.html");
+            
             driver.Manage().Window.Maximize();
+            
             System.Threading.Thread.Sleep(1000);
             IWebElement matricula = driver.FindElement(By.Name("S48_"));
             matricula.SendKeys("600049");
+            
             System.Threading.Thread.Sleep(1000);
-
             IWebElement senha = driver.FindElement(By.Name("S62_"));
-            senha.SendKeys("calor");
+            senha.SendKeys("SuaSenhaAqui");
+            
             System.Threading.Thread.Sleep(1000);
-
             IWebElement tipo = driver.FindElement(By.Name("S76_"));
             tipo.SendKeys("g");
+            
             System.Threading.Thread.Sleep(1000);
-
             IWebElement clicar = driver.FindElement(By.Name("S122_"));
             clicar.Click();
-
 
             System.Threading.Thread.Sleep(5000);
             driver.Quit();
